cf81d46d23949388c903161ae7c907baadc7d06a
SixthSurge <surgecrafter@gmail.com>
2023-05-07 11:30:53 +0100

    Update gbuffers_basic

--- shaders/program/gbuffers/basic.glsl
+++ shaders/program/gbuffers/basic.glsl
@@ -93,19 +93,10 @@ void main() {
 //----------------------------------------------------------------------------//
 #if defined fsh
 
-layout (location = 0) out vec4 base_color;
+/* DRAWBUFFERS:012 */
+layout (location = 0) out vec3 scene_color;
 layout (location = 1) out vec4 gbuffer_data_0; // albedo, block ID, flat normal, light levels
 layout (location = 2) out vec4 gbuffer_data_1; // detailed normal, specular map (optional)
-
-/* DRAWBUFFERS:31 */
-
-#ifdef NORMAL_MAPPING
-/* DRAWBUFFERS:312 */
-#endif
-
-#ifdef SPECULAR_MAPPING
-/* DRAWBUFFERS:312 */
-#endif
 
 flat in vec2 light_levels;
 flat in vec3 tint;
@@ -117,6 +108,7 @@ flat in vec3 tint;
 uniform vec2 view_res;
 uniform vec2 view_pixel_size;
 
+#include "/include/utility/color.glsl"
 #include "/include/utility/encoding.glsl"
 
 const vec3 normal = vec3(0.0, 1.0, 0.0);
@@ -126,6 +118,9 @@ void main() {
 	vec2 coord = gl_FragCoord.xy * view_pixel_size * rcp(taau_render_scale);
 	if (clamp01(coord) != coord) discard;
 #endif
+
+
+	scene_color = srgb_eotf_inv(tint) * rec709_to_working_color;
 
 	vec2 encoded_normal = encode_unit_vector(normal);
 
@@ -142,12 +137,6 @@ void main() {
 	const vec4 specular_map = vec4(0.0);
 	gbuffer_data_1.z = pack_unorm_2x8(specular_map.xy);
 	gbuffer_data_1.w = pack_unorm_2x8(specular_map.zw);
-#endif
-
-#if defined IS_IRIS && defined PROGRAM_GBUFFERS_LINE
-	base_color = vec4(tint, 1.0);
-#else
-	base_color = vec4(0.0);
 #endif
 }
 
