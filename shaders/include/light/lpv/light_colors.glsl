92e4c9dfe2e4b813e58ad6f64f06b9af2a651855
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2023-09-21 19:19:38 +0100

    Weaker coloured lights

--- shaders/include/light/lpv/light_colors.glsl
+++ shaders/include/light/lpv/light_colors.glsl
@@ -2,37 +2,37 @@
 #define INCLUDE_LIGHT_LPV_LIGHT_COLORS
 
 const vec3[32] light_color = vec3[32](
-	vec3(1.00, 1.00, 1.00) * 12.0, // Strong white light
-	vec3(1.00, 1.00, 1.00) *  6.0, // Medium white light
-	vec3(1.00, 1.00, 1.00) *  1.0, // Weak white light
-	vec3(1.00, 0.55, 0.27) * 12.0, // Strong golden light
-	vec3(1.00, 0.57, 0.30) *  8.0, // Medium golden light
-	vec3(1.00, 0.57, 0.30) *  4.0, // Weak golden light
-	vec3(1.00, 0.18, 0.10) *  5.0, // Redstone components
-	vec3(1.00, 0.30, 0.10) * 24.0, // Lava
-	vec3(1.00, 0.45, 0.10) *  9.0, // Medium orange light
-	vec3(1.00, 0.63, 0.15) *  4.0, // Brewing stand
-	vec3(1.00, 0.57, 0.30) * 12.0, // Medium golden light
-	vec3(0.45, 0.73, 1.00) *  6.0, // Soul lights
-	vec3(0.45, 0.73, 1.00) * 14.0, // Beacon
-	vec3(0.75, 1.00, 0.83) *  3.0, // Sculk
-	vec3(0.75, 1.00, 0.83) *  1.0, // End portal frame
-	vec3(0.60, 0.10, 1.00) *  4.0, // Pink glow
-	vec3(0.75, 1.00, 0.50) *  1.0, // Sea pickle
-	vec3(1.00, 0.50, 0.25) *  4.0, // Nether plants
-	vec3(1.00, 0.57, 0.30) *  8.0, // Medium golden light
-	vec3(1.00, 0.65, 0.30) *  8.0, // Ochre froglight
-	vec3(0.86, 1.00, 0.44) *  8.0, // Verdant froglight
-	vec3(0.75, 0.44, 1.00) *  8.0, // Pearlescent froglight
-	vec3(0.60, 0.10, 1.00) *  2.0, // Enchanting table
-	vec3(0.75, 0.44, 1.00) *  4.0, // Amethyst cluster
-	vec3(0.75, 0.44, 1.00) *  4.0, // Calibrated sculk sensor
-	vec3(0.75, 1.00, 0.83) *  6.0, // Active sculk sensor
-	vec3(1.00, 0.18, 0.10) *  3.3, // Redstone block
+	vec3(1.00, 1.00, 1.00) * 2.0, // Strong white light
+	vec3(1.00, 1.00, 1.00) *  1.0, // Medium white light
+	vec3(1.00, 1.00, 1.00) *  0.25, // Weak white light
+	vec3(1.00, 0.55, 0.27) * 1.0, // Strong golden light
+	vec3(1.00, 0.57, 0.30) *  0.6, // Medium golden light
+	vec3(1.00, 0.57, 0.30) *  0.3, // Weak golden light
+	vec3(1.00, 0.18, 0.10) *  1.75, // Redstone components
+	vec3(1.00, 0.30, 0.10) * 16.0, // Lava
+	vec3(1.00, 0.45, 0.10) *  2.25, // Medium orange light
+	vec3(1.00, 0.63, 0.15) *  1.0, // Brewing stand
+	vec3(1.00, 0.57, 0.30) * 3.0, // Medium golden light
+	vec3(0.45, 0.73, 1.00) *  1.5, // Soul lights
+	vec3(0.45, 0.73, 1.00) * 4.5, // Beacon
+	vec3(0.75, 1.00, 0.83) *  0.75, // Sculk
+	vec3(0.75, 1.00, 0.83) *  0.25, // End portal frame
+	vec3(0.60, 0.10, 1.00) *  1.0, // Pink glow
+	vec3(0.75, 1.00, 0.50) *  0.25, // Sea pickle
+	vec3(1.00, 0.50, 0.25) *  1.0, // Nether plants
+	vec3(1.00, 0.57, 0.30) *  2.0, // Medium golden light
+	vec3(1.00, 0.65, 0.30) *  2.0, // Ochre froglight
+	vec3(0.86, 1.00, 0.44) *  2.0, // Verdant froglight
+	vec3(0.75, 0.44, 1.00) *  2.0, // Pearlescent froglight
+	vec3(0.60, 0.10, 1.00) *  0.5, // Enchanting table
+	vec3(0.75, 0.44, 1.00) *  1.0, // Amethyst cluster
+	vec3(0.75, 0.44, 1.00) *  1.0, // Calibrated sculk sensor
+	vec3(0.75, 1.00, 0.83) *  1.5, // Active sculk sensor
+	vec3(1.00, 0.18, 0.10) *  0.85, // Redstone block
 	vec3(0.0), // Unused
 	vec3(0.0), // Unused
 	vec3(0.0), // Unused
-	vec3(0.60, 0.10, 1.00) * 12.0, // Nether portal
+	vec3(0.60, 0.10, 1.00) * 6.0, // Nether portal
 	vec3(0.0)  // End portal
 );
 
