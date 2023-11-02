575082b1b2df89b9e1fd5e95984e89be7c6cb722
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2023-11-02 20:53:35 +0000

    Light colour tweaks

--- shaders/include/light/lpv/light_colors.glsl
+++ shaders/include/light/lpv/light_colors.glsl
@@ -5,16 +5,16 @@ const vec3[32] light_color = vec3[32](
 	vec3(1.00, 1.00, 1.00) * 1.25, // Strong white light
 	vec3(1.00, 1.00, 1.00) *  1.0, // Medium white light
 	vec3(1.00, 1.00, 1.00) *  0.25, // Weak white light
-	vec3(1.00, 0.55, 0.27) * 1.0, // Strong golden light
-	vec3(1.00, 0.57, 0.30) *  0.6, // Medium golden light
-	vec3(1.00, 0.57, 0.30) *  0.3, // Weak golden light
+	vec3(1.00, 0.45, 0.20) * 1.0, // Strong golden light
+	vec3(1.00, 0.40, 0.15) *  0.6, // Medium golden light
+	vec3(1.00, 0.40, 0.15) *  0.3, // Weak golden light
 	vec3(1.00, 0.18, 0.10) *  1.75, // Redstone components
 	vec3(1.00, 0.18, 0.00) * 10.0, // Lava
 	vec3(1.00, 0.45, 0.10) *  2.25, // Medium orange light
 	vec3(1.00, 0.63, 0.15) *  1.0, // Brewing stand
 	vec3(1.00, 0.57, 0.30) * 0.6, // Medium golden light
-	vec3(0.45, 0.73, 1.00) *  1.1, // Soul lights
-	vec3(0.45, 0.73, 1.00) * 4.5, // Beacon
+	vec3(0.05, 0.23, 1.00) *  1.1, // Soul lights
+	vec3(0.45, 0.53, 1.00) * 4.5, // Beacon
 	vec3(0.75, 1.00, 0.83) *  0.75, // Sculk
 	vec3(0.75, 1.00, 0.83) *  0.25, // End portal frame
 	vec3(0.60, 0.10, 1.00) *  0.6, // Pink glow
@@ -28,7 +28,7 @@ const vec3[32] light_color = vec3[32](
 	vec3(0.75, 0.44, 1.00) *  1.0, // Amethyst cluster
 	vec3(0.75, 0.44, 1.00) *  1.0, // Calibrated sculk sensor
 	vec3(0.75, 1.00, 0.83) *  1.5, // Active sculk sensor
-	vec3(1.00, 0.18, 0.10) *  0.85, // Redstone block
+	vec3(1.00, 0.00, 0.00) *  1.9, // Redstone block
 	vec3(0.0), // Unused
 	vec3(0.0), // Unused
 	vec3(0.0), // Unused
