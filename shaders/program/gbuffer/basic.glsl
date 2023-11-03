fafae2dac005c3bad43a3f207d85b538d3f66fe8
tsumi <who@asked>
2023-11-03 13:40:36 +0100

    basic: support translucent geometry
    gbuffers_basic is abused by mods (such as litematica) for rendering
    overlays and the like, it needs to support translucency to not break
    this.

--- shaders/program/gbuffer/basic.glsl
+++ shaders/program/gbuffer/basic.glsl
@@ -16,14 +16,14 @@
 #if defined vsh
 
 flat out vec2 light_levels;
-flat out vec3 tint;
+flat out vec4 tint;
+in vec4  vaColor;
 
 #ifdef PROGRAM_GBUFFERS_LINE
 // gbuffers_line seems to break in Iris 1.6 when the compatibility profile is
 // used, so we must use the core spec
 in vec3  vaPosition;
 in vec3  vaNormal;
-in vec4  vaColor;
 in ivec2 vaUV2;
 
 uniform mat4 projectionMatrix;
@@ -41,7 +41,6 @@ uniform vec2 view_pixel_size;
 void main() {
 #if defined PROGRAM_GBUFFERS_LINE
 	light_levels = clamp01(vec2(vaUV2) * rcp(240.0));
-	tint = vaColor.rgb;
 
 	// Taken from Minecraft 1.17's rendertype_lines.vsh
 
@@ -77,8 +76,6 @@ void main() {
 		: vec4((ndc1 - vec3(line_offset, 0.0)) * line_pos_start.w, line_pos_start.w);
 #else
 	light_levels = clamp01(gl_MultiTexCoord1.xy * rcp(240.0));
-	tint = gl_Color.rgb;
-
 	vec3 view_pos = transform(gl_ModelViewMatrix, gl_Vertex.xyz);
 	vec4 clip_pos = project(gl_ProjectionMatrix, view_pos);
 #endif
@@ -90,6 +87,7 @@ void main() {
 	clip_pos.xy += taa_offset * clip_pos.w * 0.66;
 #endif
 
+	tint = vaColor;
 	gl_Position = clip_pos;
 }
 
@@ -101,14 +99,14 @@ void main() {
 //----------------------------------------------------------------------------//
 #if defined fsh
 
-layout (location = 0) out vec3 scene_color;
+layout (location = 0) out vec4 scene_color;
 layout (location = 1) out vec4 gbuffer_data_0; // albedo, block ID, flat normal, light levels
 layout (location = 2) out vec4 gbuffer_data_1; // detailed normal, specular map (optional)
 
 /* DRAWBUFFERS:012 */
 
 flat in vec2 light_levels;
-flat in vec3 tint;
+flat in vec4 tint;
 
 // ------------
 //   Uniforms
@@ -129,11 +127,23 @@ uniform float frameTimeCounter;
 
 const vec3 normal = vec3(0.0, 1.0, 0.0);
 
+// gbuffers_basic is abused by mods for rendering overlays and the like, for now we mostly just
+// want to reduce the intensity of these translucent overlays. This shader and vanilla are still
+// both affected by Z-fighting in some areas, we could attempt to fix this in the vertex
+// shader but this would require mods to send correct normals for their geometry which unfortunately
+// doesn't happen often.
+float fixup_translucent(float alpha) {
+    return mix(alpha * 0.25, alpha, step(0.9, alpha));
+}
+
 void main() {
 #if defined TAA && defined TAAU
 	vec2 coord = gl_FragCoord.xy * view_pixel_size * rcp(taau_render_scale);
 	if (clamp01(coord) != coord) discard;
 #endif
+	// I have yet to see anything render something transparent but we assume it can happen.
+	if (tint.a < 0.1)
+		discard;
 
 #if defined PROGRAM_GBUFFERS_LINE && BOX_MODE != BOX_MODE_NONE
 	if (renderStage == MC_RENDER_STAGE_OUTLINE) {
@@ -143,10 +153,13 @@ void main() {
 		vec2  uv       = gl_FragCoord.xy * view_pixel_size;
 		vec3  col      = hsl_to_rgb(vec3(fract(uv.y + uv.x * uv.y + frameTimeCounter * 0.1), 1.0, 1.0));
 #endif
-		scene_color    = srgb_eotf_inv(col * vec3(1.0 + BOX_EMISSION)) * rec709_to_working_color;
+		scene_color.rgb = srgb_eotf_inv(col * vec3(1.0 + BOX_EMISSION)) * rec709_to_working_color;
 	} else // careful editing scene_color below
 #endif
-	scene_color = srgb_eotf_inv(tint) * rec709_to_working_color;
+	scene_color.rgb = srgb_eotf_inv(tint.rgb) * rec709_to_working_color;
+
+	// see note in function
+	scene_color.a = fixup_translucent(tint.a);
 
 	vec2 encoded_normal = encode_unit_vector(normal);
 
