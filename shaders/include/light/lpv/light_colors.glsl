9caea24af16e3b77e20d90dba3c83f97c65e6401
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2023-12-25 23:57:28 +0000

    Revert auto exposure related changes
    This reverts commit dcf5dadfbb00ec8dc8abb6434a89c991dcfc901f.
    
    Revert "Calm your auto exposure"
    
    This reverts commit 225882c40d4d8a12f03182fa3995e52b67ac9cf0.
    
    Revert "Account light colours for auto exposure"
    
    This reverts commit be9f53e6e8f340a4e7e2c020f414dcade0ae59f4.
    
    Revert "Auto exposure actually does something"
    
    This reverts commit e3ed294bc02fdfe6ac8142b2f92417b6edfbbde0.
    
    Revert "Make golden light more golden"
    
    This reverts commit d652c6fbcab8f6361e26d58692b44ba52831744b.

--- shaders/include/light/lpv/light_colors.glsl
+++ shaders/include/light/lpv/light_colors.glsl
@@ -5,14 +5,14 @@ const vec3[32] light_color = vec3[32](
 	vec3(1.00, 1.00, 1.00) * 1.50, // Strong white light
 	vec3(0.90, 0.30, 1.00) *  1.25, // Medium white light
 	vec3(1.00, 1.00, 1.00) *  0.25, // Weak white light
-	vec3(1.00, 0.55, 0.20) * 1.50, // Strong golden light
-	vec3(1.00, 0.70, 0.35) *  0.3, // Medium golden light
-	vec3(1.00, 0.40, 0.15) *  0.15, // Weak golden light
+	vec3(1.00, 0.45, 0.20) * 1.50, // Strong golden light
+	vec3(1.00, 0.40, 0.15) *  0.8, // Medium golden light
+	vec3(1.00, 0.40, 0.15) *  0.4, // Weak golden light
 	vec3(1.00, 0.18, 0.10) *  1.75, // Redstone components
 	vec3(1.00, 0.32, 0.00) * 8.0, // Lava
-	vec3(1.00, 0.45, 0.10) *  3.25, // Medium orange light
+	vec3(1.00, 0.45, 0.10) *  2.25, // Medium orange light
 	vec3(1.00, 0.63, 0.15) *  1.0, // Brewing stand
-	vec3(1.00, 0.70, 0.35) *  0.3, // Medium golden light
+	vec3(1.00, 0.57, 0.30) * 0.6, // Medium golden light
 	vec3(0.05, 0.23, 1.00) *  1.1, // Soul lights
 	vec3(0.45, 0.53, 1.00) * 4.5, // Beacon
 	vec3(0.75, 1.00, 0.83) *  0.75, // Sculk
@@ -20,7 +20,7 @@ const vec3[32] light_color = vec3[32](
 	vec3(0.60, 0.10, 1.00) *  0.6, // Pink glow
 	vec3(0.75, 1.00, 0.50) *  0.25, // Sea pickle
 	vec3(1.00, 0.50, 0.25) *  1.0, // Nether plants
-	vec3(1.00, 0.70, 0.35) *  0.3, // Medium golden light
+	vec3(1.00, 0.57, 0.30) *  0.6, // Medium golden light
 	vec3(1.00, 0.65, 0.30) *  0.9, // Ochre froglight
 	vec3(0.76, 1.00, 0.34) *  0.9, // Verdant froglight
 	vec3(0.75, 0.44, 1.00) *  0.9, // Pearlescent froglight
