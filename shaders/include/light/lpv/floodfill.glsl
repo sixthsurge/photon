e7c2f321890d905ff0e8e5ef4dce145c3871b832
sixthsurge <surgecrafter@gmail.com>
2024-04-08 16:37:45 +0100

    floodfill - automatic transparent block detection

--- shaders/include/light/lpv/floodfill.glsl
+++ shaders/include/light/lpv/floodfill.glsl
@@ -11,21 +11,6 @@ bool is_translucent(uint block_id) {
 	return 64u <= block_id && block_id < 80u;
 }
 
-bool is_transparent(uint block_id) {
-	return block_id == 0u  || // Air
-	       block_id == 2u  || // Small plants
-	       block_id == 3u  || // Tall plants (lower half)
-		   block_id == 4u  || // Tall plants (upper half)
-	       block_id == 5u  || // Leaves
-	       block_id == 14u || // Strong SSS
-	       block_id == 15u || // Weak SSS
-	       block_id == 18u || // Transparent metal objects
-	       block_id == 41u || // Brewing stand
-	       block_id == 48u || // Sea pickle
-	       block_id == 49u || // Nether mushrooms
-	       block_id == 80u;   // Miscellaneous transparent
-}
-
 vec3 get_emitted_light(uint block_id) {
 	if (is_emitter(block_id)) {
 		return texelFetch(light_data_sampler, ivec2(int(block_id) - 32, 0), 0).rgb;
@@ -34,11 +19,11 @@ vec3 get_emitted_light(uint block_id) {
 	}
 }
 
-vec3 get_tint(uint block_id) {
+vec3 get_tint(uint block_id, bool is_transparent) {
 	if (is_translucent(block_id)) {
 		return texelFetch(light_data_sampler, ivec2(int(block_id) - 64, 1), 0).rgb;
 	} else {
-		return vec3(is_transparent(block_id));
+		return vec3(is_transparent);
 	}
 }
 
@@ -69,10 +54,12 @@ void update_lpv(writeonly image3D light_img, sampler3D light_sampler) {
 	ivec3 pos = ivec3(gl_GlobalInvocationID);
 	ivec3 previous_pos = ivec3(vec3(pos) - floor(previousCameraPosition) + floor(cameraPosition));
 
-	uint block_id      = texelFetch(voxel_sampler, pos, 0).x;
-	vec3 light_avg     = gather_light(light_sampler, previous_pos) * rcp(7.0);
-	vec3 emitted_light = sqr(get_emitted_light(block_id));
-	vec3 tint          = sqr(get_tint(block_id));
+	uint block_id       = texelFetch(voxel_sampler, pos, 0).x;
+	bool transparent    = block_id == 0u || block_id >= 128u;
+	block_id            = block_id & 127;
+	vec3 light_avg      = gather_light(light_sampler, previous_pos) * rcp(7.0);
+	vec3 emitted_light  = sqr(get_emitted_light(block_id));
+	vec3 tint           = sqr(get_tint(block_id, transparent));
 
 	vec3 light = emitted_light + light_avg * tint;
 
--- shaders/include/light/lpv/voxelization.glsl
+++ shaders/include/light/lpv/voxelization.glsl
@@ -17,14 +17,41 @@ bool is_inside_voxel_volume(vec3 voxel_pos) {
 }
 
 #ifdef PROGRAM_SHADOW
+bool is_voxelized(uint block_id, bool vertex_at_grid_corner) {
+	bool is_terrain = any(equal(ivec4(renderStage), ivec4(MC_RENDER_STAGE_TERRAIN_SOLID, MC_RENDER_STAGE_TERRAIN_TRANSLUCENT, MC_RENDER_STAGE_TERRAIN_CUTOUT, MC_RENDER_STAGE_TERRAIN_CUTOUT_MIPPED)));
+
+	bool is_transparent_block =
+	    block_id == 0u  || // Air
+		block_id == 1u  || // Water
+	    block_id == 5u  || // Leaves
+	    block_id == 14u || // Strong SSS
+	    block_id == 15u || // Weak SSS
+	    block_id == 18u || // Transparent metal objects
+	    block_id == 80u;   // Miscellaneous transparent
+	
+	bool is_light_emitting_block = 32u <= block_id && block_id < 64u;
+
+	return (vertex_at_grid_corner || is_light_emitting_block) && is_terrain && !is_transparent_block;
+}
+
+// Returns true if pos is within `tolerance` of a corner of the unit cube
+bool is_corner(vec3 pos, float tolerance) {
+	return all(lessThan(pos, vec3(tolerance)) || greaterThan(pos, vec3(1.0 - tolerance)));
+}
+
 void update_voxel_map(uint block_id) {
 	vec3 model_pos = gl_Vertex.xyz + at_midBlock * rcp(64.0);
 	vec3 view_pos  = transform(gl_ModelViewMatrix, model_pos);
 	vec3 scene_pos = transform(shadowModelViewInverse, view_pos);
 	vec3 voxel_pos = scene_to_voxel_space(scene_pos);
 
-	bool is_terrain = any(equal(ivec4(renderStage), ivec4(MC_RENDER_STAGE_TERRAIN_SOLID, MC_RENDER_STAGE_TERRAIN_TRANSLUCENT, MC_RENDER_STAGE_TERRAIN_CUTOUT, MC_RENDER_STAGE_TERRAIN_CUTOUT_MIPPED)));
-	bool is_water = block_id == 1;
+	// Work out whether this vertex is in the lower corner of the block grid
+	vec3 block_pos = transform(gl_ModelViewMatrix, gl_Vertex.xyz);
+	     block_pos = transform(shadowModelViewInverse, block_pos);
+		 block_pos = fract(block_pos + cameraPosition);
+	bool vertex_at_grid_corner = is_corner(block_pos, rcp(16.0) - 1e-3);
+
+	bool is_voxelized = is_voxelized(block_id, vertex_at_grid_corner);
 
 	// Prevent blocks that aren't part of another category in shaders.properties from being treated as air
 	block_id = max(block_id, 1u);
@@ -35,7 +62,12 @@ void update_voxel_map(uint block_id) {
 	block_id = block_id * (1u - is_warped_stem) + 46 * is_warped_stem;
 	block_id = block_id * (1u - is_crimson_stem) + 58 * is_crimson_stem;
 
-	if (is_terrain && is_inside_voxel_volume(voxel_pos) && !is_water) {
+	// Mark transparent light sources
+	block_id = (vertex_at_grid_corner)
+		? block_id
+		: clamp(block_id + 128u, 0u, 255u);
+
+	if (is_voxelized && is_inside_voxel_volume(voxel_pos)) {
 		imageStore(voxel_img, ivec3(voxel_pos), uvec4(block_id, 0u, 0u, 0u));
 	}
 }
