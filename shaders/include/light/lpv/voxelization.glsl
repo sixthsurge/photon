2396473f9ac7863f97fc1af1223d36b9b20855f2
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2024-04-20 12:37:21 +0100

    Add carpets as transparent

--- shaders/include/light/lpv/voxelization.glsl
+++ shaders/include/light/lpv/voxelization.glsl
@@ -23,6 +23,7 @@ bool is_voxelized(uint block_id, bool vertex_at_grid_corner) {
 	bool is_transparent_block =
 		block_id == 1u  || // Water
 	    block_id == 18u || // Transparent metal objects
+	    block_id == 27u || // Carpets
 	    block_id == 80u;   // Miscellaneous transparent
 
 	bool is_light_emitting_block = 32u <= block_id && block_id < 64u;
