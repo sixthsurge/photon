4f9a889d694e622bad1ad8d5d6911bf69e0936a1
Essentuan <essentuan@gmail.com>
2026-04-13 16:18:54 -0500

    Respect handheld options for Photonics handheld

--- shaders/photonics/modifiers/light_modifier.glsl
+++ shaders/photonics/modifiers/light_modifier.glsl
@@ -1,3 +1,7 @@
 void modify_light(inout Light light, vec3 world_pos) {
-    light.color*= BLOCKLIGHT_I;
+    if (light.index < 0) {
+        light.color *= HANDHELD_LIGHTING_INTENSITY;
+    } else {
+        light.color *= BLOCKLIGHT_I;
+    }
 }
