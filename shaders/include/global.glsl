5c377dd9d7a7b99059f10dfe3339500a5769a0f8
David Huynh <xgarhontx@gmail.com>
2026-05-27 18:13:45 -0700

    Added lottes_emulated tonemap

--- shaders/include/global.glsl
+++ shaders/include/global.glsl
@@ -58,9 +58,14 @@
 #define PHOTONICS_IN_USE
 #endif
 
-// Allow user to disable HDR in config menu, even if HDR mod is installed
-#ifndef PHOTON_HDR_ENABLED
-#undef HDR_ENABLED
+// HDRMod forced define & values for devs
+#ifndef HDR_ENABLED
+#ifdef HDR_ENABLED_ALLOW
+#define HDR_ENABLED
+#endif
+const float HdrGamePeakBrightness = 1000.f;
+const float HdrGamePaperWhiteBrightness = 203.f;
+const float HdrUIBrightness = 203.f;
 #endif
 
 // Compatibility fixes
